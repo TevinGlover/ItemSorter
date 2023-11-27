952959d0481612a325a7e75e282a2b5e6720a8e0
Tevin Glover <tevwork.tg@gmail.com>
2023-11-27 08:28:19 -0500

    working on it

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,28 +4,34 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+long id;
+String name;
+Double price;
     public Item() {
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
     public void setPrice(Double price) {
